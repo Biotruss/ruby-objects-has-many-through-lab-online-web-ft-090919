00eb1bdc82640e8476c9ae5135ab0a871d3a2f7f
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-26 04:16:31 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,40 +1,16 @@
-class Transfer
-  attr_reader :amount, :sender, :receiver
-  attr_accessor :status
+class Appointment
+  attr_accessor :date, :doctor, :patient
 
-  def initialize(sender, receiver, amount)
-    @status = "pending"
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
   end
 
-  def valid?
-    sender.valid? && receiver.valid?
-  end
-
-  def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount
-      self.status = "complete"
-    else
-      reject_transfer
-    end
-  end
-
-  def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
-      self.status = "reversed"
-    else
-      reject_transfer
-    end
-  end
-
-  def reject_transfer
-    self.status = "rejected"
-    "Transaction rejected. Please check your account balance."
+  def self.all
+    @@all
   end
 end
